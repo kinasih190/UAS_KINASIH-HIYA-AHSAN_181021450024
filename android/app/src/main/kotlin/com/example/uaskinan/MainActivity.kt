cf957d79393b3da8a012c996d081fb8df839840e
kinasih190 <91204471+kinasih190@users.noreply.github.com>
2021-10-17 00:52:48 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/uaskinan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uaskinan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'weather_bloc.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (context) => WeatherBloc(),
+        child: HomeScreen(),
+      ),
+    );
+  }
+}
